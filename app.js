3c5deb7053215b73f6f400361064a0b56b6ced2a
kc2029 <91122465+kc2029@users.noreply.github.com>
2021-11-20 09:42:10 +0000

    V1

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require("express");
 //creating app
 const app = express();
 
-//send an HTTP response when receiving HTTP GET /
+app.use(express.static("views"));
+
+app.set("view engine", "ejs");
+app.get("/contacts", (req, res) => {
+  //res.send("test3");
+  //res.end();
+  //res.sendFile("public/index.html", { root: __dirname });
+
+  res.render("contacts"); //no need for ejs extension
+});
+
 app.get("/", (req, res) => {
-  res.sendFile("public/index.html", { root: __dirname });
+  //res.send("test3");
+  //res.end();
+  //res.sendFile("public/index.html", { root: __dirname });
+
+  res.render("index"); //no need for ejs extension
 });
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
